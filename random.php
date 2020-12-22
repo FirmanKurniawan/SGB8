4ee6ce71844f094f06bd0046d2a15fd9e089e7e0
/Mr-Roz3R <mrroz3r@gmail.com>
2020-12-22 14:34:58 +0700

    Update random.php

--- random.php
+++ random.php
@@ -20,16 +20,33 @@ $quotes=array(
 'Lebih baik menangis di tengah hujan sehingga tidak ada yang bisa mengerti bahwa kamu bahagia atau kesakitan.',
 'Jangan pernah berhenti menunjukkan kepada seseorang betapa mereka sangat berarti bagimu.',
 'Saya menangis karena saya tidak punya sepatu, lalu saya bertemu dengan seorang pria yang tidak punya kaki.',
-'Kuhancurkan tulang-tulangku, tetapi aku tidak membuangnya, sampai aku mendengar suara cinta memanggilku dan melihat jiwaku siap untuk berpetualang.',
-'Berasumsi dengan perasaan, sama saja dengan membiarkan hatimu diracuni harapan baik, padahal boleh jadi kenyataan tidak seperti itu, menyakitkan.',
-'Sebab, aku tahu cinta terbaik akan selalu pulang. Jika kau tak kunjung datang, barangkali kau hanya ditakdirkan sebagai kisah yang hanya layak tersimpan sebagai kenangan.',
-'Kadang, yang terindah tak diciptakan untuk dimiliki. Cukup dipandangi dari jauh, lalu disyukuri bahwa ia ada di sana untuk dikagumi dalam diam.',
 'Selalu ada hari baru untuk setiap napas. Selalu ada kesempatan baru untuk kembali tersenyum. Patah hati tidak harus selamanya, kan?',
 'Kamu boleh jatuh cinta kepada siapa saja, tapi kamu tidak punya hak memaksakan orang lain mencintaimu.',
-'Begitulah perasaan, saat dia memilih jatuh di hatimu, kau hanya punya dua pilihan. Mengambilnya dan menyatakan, atau membiarkan waktu membuatnya hilang dan mungkin diambil orang lain.',
-'Tugasmu bukan untuk mencari cinta, tapi hanya untuk mencari dan menemukan semua hambatan dalam dirimu yang secara tidak sadar telah kau bangun untuk melawan cinta.',
 'Hidup tanpa cinta layaknya pohon tanpa bunga dan buah.',
-'Kau akan tahu bahwa kau sedang jatuh cinta ketika kau tidak ingin jatuh tertidur karena realitas tampak lebih indah daripada mimpi.'
+'Kau akan tahu bahwa kau sedang jatuh cinta ketika kau tidak ingin jatuh tertidur karena realitas tampak lebih indah daripada mimpi.',
+'Cewek itu paling pinter nutup-nutupin! Kalo dia lg sedih, dia slalu bs nunjukkin dirinya gapapa.. Padahal mah dia lg kenapa-kenapa.',
+'Trauma adalah bagian dari rasa sakit yg terlalu dalam.',
+'Sekuat-kuatnya orang kalo di sakitin terus pasti bakalan rapuh juga.',
+'Saya bukannya tidak punya airmata, saya hanya menahan airmata untuk tidak mengalir.',
+'Perasaan aku ke kamu daridulu gak berubah2! Msh tetep syg sama kamu.. Walaupun kadang kamu itu nyakitin banget.',
+'Maafin aku yaa.. Aku gagal jadi yg terbaik buat kamu.',
+'Kalo harus jadi orang jahat, aku bisa aja jahat sama kamu! Tapi aku masih mikirin perasaan oranglain. Gak kaya kamu!',
+'Diamnya aku bukan berarti aku gak tau apa-apa! Tapi walaupun diam, aku selalu mantau kamu disini.',
+'Sebutin nama orang-orang yg lo sayang. ',
+'Semua orang pasti punya masalalu. Tapi gak seharusnya lo stuck di masalalu lo itu!',
+'Sabar teruss.. Sampe orang yg di sabarin sadar!',
+'Maafin aku. Aku blm bs jd yg terbaik buat kamu. Tapi aku slalu berusaha! Dan kamu, hanya menganggap usaha aku sebelah mata saja.',
+'Cuma aku dan Tuhan yg tau gimana perasaan aku ke kamu.',
+'Move on itu nggak bakal ada habisnya! contoh: udh bs move on dr yg ini, eh ntar pacaran, putus lg, trs susah buat move on dr yg itu.',
+'Cewek itu jarang bisa marah sama orang yg dia sayang. Terkadang emosi nya hanya bisa di luapkan lewat "air mata".',
+'Orang cakep, banyak! Tapi orang cakep, baik+setia jarang-jarang.',
+'Gue jutek juga sebenernya sayang sama lo.',
+'Pertahankan orang yg pantes buat lo pertahanin. Jgn mempertahankan orang yg salah!',
+'Ngomong sih gampang, ngelakuinnya itu nggak segampang ngomong.',
+'Sekali sayang, tetep sayang.',
+'Maaf kalo aku ngebetein terus.',
+'Satnight mau kemana nih?', 
+'Kangen banget sebenernya.. Tapi gitudeh.'
 );
 
 while(true)
@@ -47,13 +64,13 @@ $headers[] = "Authority: twitter.com";
 $headers[] = "X-Requested-With: XMLHttpRequest";
 $headers[] = "X-Twitter-Active-User: yes";
 $url = 'https://twitter.com/i/tweet/create';
-$post = 'authenticity_token='.$auth.'&batch_mode=off&is_permalink_page=false&place_id=&status='.$quotes[rand(0, 22)].' ('.rand(1111,9999).') Happy+anniversary+yang+ke+8th+%F0%9F%8E%89%0A%23AnniversarySGBTeam8th%0A%23SGBTeam%0A%23SGBTeamAnniversary8th%0A%23SGBTeamAnniversary8&tagged_users=';
+$post = 'authenticity_token='.$auth.'&batch_mode=off&is_permalink_page=false&place_id=&status='.$quotes[rand(0, 30)].' ('.rand(1111,9999).') Happy+anniversary+yang+ke+8th+%F0%9F%8E%89%0A%23AnniversarySGBTeam8th%0A%23SGBTeam%0A%23SGBTeamAnniversary8th%0A%23SGBTeamAnniversary8&tagged_users=';
 
 $post = json_decode(yarzCurl($url, $post, false, $headers, true));
 if(isset($post->tweet_id))
 {
     echo "Tweet ID : ".$post->tweet_id."\n";
-	sleep(50);
+	sleep(5);
 } else {
 	die(print_r($post));
 }
